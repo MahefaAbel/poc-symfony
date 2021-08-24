d09fb51584eb78579e829fc17a63e96827f2da93
Mahefa Abel <abelmahefa@gmail.com>
2021-08-24 17:04:31 +0300

    update

---
+++ migrations/migration.php
@@ -0,0 +1,6 @@
+<!-- $this->addSql('
+            INSERT INTO `user` (`email`, `roles`, `password`)
+            VALUES
+                (\'admin@admin.com\', \'["ROLE_ADMIN"]\', \'$argon2id$v=19$m=65536,t=4,p=1$vXDWeyV4UpECkr6/FPbPzw$nz3qqX3kGOiojADO65zWR3xo5FMDcPjgPECZGFDhA1I\');
+            '
+        ); -->
